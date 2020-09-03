f52a2625f19654af6fb5845d84dd93bf89438226
jayruin <28711665+jayruin@users.noreply.github.com>
2020-09-03 17:33:57 -0400

    functions: Statements and expressions.

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,16 @@
 fn main() {
-    another_function(5, 6);
-}
-
-fn another_function( x: i32, y: i32) {
-    println!("The value of x is {}", x);
+    // another_function(5, 6);
+    // let x = (let y = 6); // Error since statements do not return anything
+    // Block for new scopes is an expression
+    // let x = 5;
+    let y = {
+        let x = 3;
+        x + 1 // Note the lack of semicolon for expressions
+    };
     println!("The value of y is {}", y);
 }
+
+// fn another_function( x: i32, y: i32) {
+//     println!("The value of x is {}", x);
+//     println!("The value of y is {}", y);
+// }
