cdaee8abdeabeb46ccb53586bd57f907c44a0279
jayruin <28711665+jayruin@users.noreply.github.com>
2020-09-03 17:20:25 -0400

    variables: Data types.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -20,4 +20,45 @@ fn main() {
     // let spaces = spaces.len();
     // let mut spaces = "    ";
     // spaces = spaces.len();
+
+    // Need to add type annotation, otherwise error
+    // let guess: u32 = "42".parse().expect("Not a number!");
+
+    // Floating-point types
+    // let x = 2.0;
+    // let y: f32 = 3.0;
+
+    // Numeric operations
+    // let sum = 5 + 10;
+    // let difference = 95.5 - 4.3;
+    // let quotient = 56.7 / 32.2;
+    // let remainder = 43 % 5;
+
+    // Boolean type
+    // let t = true;
+    // let f: bool = false;
+
+    // Character type
+    // let c = 'z';
+    // let z = 'Z';
+    // let heart_eyed_cat = '😻';
+
+    // Tuple
+    // let tup: (i32, f64, u8) = (500, 6.4, 1);
+    // let (x, y, z) = tup; // Destructuring
+    // println!("The value of y is: {}", y);
+    // let x: (i32, f64, u8) = (500, 6.4, 1);
+    // let five_hundred = x.0;
+    // let six_point_four = x.1;
+    // let one = x.2;
+
+    // Array
+    // let a: [i32; 5] = [1, 2, 3, 4, 5];
+    // let first = a[0];
+    // let second = a[1];
+    // let index = 10;
+    // let element = a[index];
+    // println!("The value of element is {}", element);
+    // let a = [3; 5];
+    // let a = [3, 3, 3, 3, 3];
 }
