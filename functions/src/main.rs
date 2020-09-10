bb837ae82e84065c7397988723a265fd2ac10437
jayruin <28711665+jayruin@users.noreply.github.com>
2020-09-10 16:40:42 -0400

    functions: Return values.

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -3,14 +3,26 @@ fn main() {
     // let x = (let y = 6); // Error since statements do not return anything
     // Block for new scopes is an expression
     // let x = 5;
-    let y = {
-        let x = 3;
-        x + 1 // Note the lack of semicolon for expressions
-    };
-    println!("The value of y is {}", y);
+    // let y = {
+    //     let x = 3;
+    //     x + 1 // Note the lack of semicolon for expressions
+    // };
+    // println!("The value of y is {}", y);
+    // Return value
+    // let x = five();
+    let x = plus_one(5);
+    println!("The value of x is {}", x);
 }
 
 // fn another_function( x: i32, y: i32) {
 //     println!("The value of x is {}", x);
 //     println!("The value of y is {}", y);
 // }
+
+// fn five() -> i32 {
+//     5
+// }
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
