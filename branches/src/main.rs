cf80fa135a50654709d919d05f6d913a643de2e1
jayruin <28711665+jayruin@users.noreply.github.com>
2020-09-10 16:46:46 -0400

    branches: if.

---
+++ branches/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let number = 3;
+    // let number = 7;
+
+    // if number < 5 {
+    //     println!("Condition was true")
+    // } else {
+    //     println!("Condition was false");
+    // }
+
+    // No implicit conversion to bool
+    // if number {
+    //     println!("Number was three");
+    // }
+    if number != 0 {
+        println!("Number was something other than zero");
+    }
+}
