da48c2937b5c6228deab1d0caa2dd5a6b8611a59
jayruin <28711665+jayruin@users.noreply.github.com>
2020-09-30 18:59:47 -0400

    loops: Finish up.

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -5,12 +5,38 @@ fn main() {
     // }
 
     // Returning values from loops
-    let mut counter = 0;
-    let result = loop {
-        counter += 1;
-        if counter == 10 {
-            break counter * 2;
-        }
-    };
-    println!("The result is {}", result);
+    // let mut counter = 0;
+    // let result = loop {
+    //     counter += 1;
+    //     if counter == 10 {
+    //         break counter * 2;
+    //     }
+    // };
+    // println!("The result is {}", result);
+
+    // Conditional loop with while
+    // let mut number = 3;
+    // while number != 0 {
+    //     println!("{}!", number);
+    //     number -= 1;
+    // }
+    // println!("LIFTOFF!!!");
+    // Looping with for
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+
+    // Looping through a collection with for
+    // let a = [10, 20, 30, 40, 50];
+    // While loop
+    // let mut index = 0;
+    // while index < 5 {
+    //     println!("the value is: {}", a[index]);
+    //     index += 1;
+    // }
+    // For loop
+    // for element in a.iter() {
+    //     println!("the value is: {}", element);
+    // }
 }
